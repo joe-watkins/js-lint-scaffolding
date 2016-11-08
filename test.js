1eaa4c28098bbbb5848d84d76fa56b6e3f7d7957
Joe Watkins <info@joe-watkins.io>
2016-11-08 15:47:57 -0800

    update package.json

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
-console.log('meow');
+Engine.ui.functionNameHere = function() {
+
+	var app = {
+
+		init: function() {
+			this.cacheDOM();
+			this.customFunction();
+		},
+		cacheDOM: function() {
+			this.$parent = $('.parent-selector');
+		},
+		cssSelectors: {
+			someCoolSelector: 'html-class-name'
+		},
+		customFunction: function() {
+			console.log('meow');
+		}
+
+		};
+
+		app.init();
+
+};
